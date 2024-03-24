8a0215321a80b072ddee771ad9a030bfd32eecba
Maharshi17Barot <99244382+Maharshi17Barot@users.noreply.github.com>
2024-03-24 11:49:30 -0400

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Batman' },
+  { id: 13, name: 'SuperMan' },
+  { id: 14, name: 'Wonder Women' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'The-Flash' },
+  { id: 19, name: 'Green-Lantern' },
+  { id: 20, name: 'Tornado' },
+];
